e87a1af95486cbd311fce65ae0d4bee4169b3b6a
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-08-22 08:40:02 +0200

    Add `get_shortcode` ajax action if it dont exists

--- bootstrap.php
+++ bootstrap.php
@@ -13,3 +13,17 @@
 add_action( 'papi/init', function () {
 	include_once __DIR__ . '/class-papi-property-shortcake.php';
 } );
+
+// Add shortcode ajax action for backward compatibility.
+// This action does only exist in Papi 3.2+
+if ( ! has_action( 'papi/ajax/get_shortcode' ) ) {
+	add_action( 'papi/ajax/get_shortcode', function () {
+		$shortcode = papi_get_qs( 'shortcode' ) ?: '';
+		$shortcode = html_entity_decode( $shortcode );
+		$shortcode = wp_unslash( $shortcode );
+
+		wp_send_json( [
+			'html' => do_shortcode( $shortcode )
+		] );
+	} );
+}
